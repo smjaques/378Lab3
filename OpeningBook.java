58eaf07c3847eb59f5a79358b7da88204b573dfb
smjaques <sjaques@calpoly.edu>
2021-02-12 21:21:54 -0800

    fixed playLoop()

--- OpeningBook.java
+++ OpeningBook.java
@@ -16,7 +16,7 @@ public class OpeningBook extends Levels
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super();
         setBackground(new GreenfootImage("openingBook.png"));
-        knightBackgroundMusic.play();
+        knightBackgroundMusic.playLoop();
     }
 
     
